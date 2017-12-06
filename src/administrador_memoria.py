6bb6d1e761607f9dd2ac69bb22b7649a1c141764
m4r103 <mariolopez.lopez66@gmail.com>
2017-12-06 16:21:04 -0600

    Agregados nuevas funciones y conexion.
    Se ha agregado las funciones de mover a swap y ram, también se ha agregado la capacidad de comnucación con el proceso creador de procesos.

--- src/administrador_memoria.py
+++ src/administrador_memoria.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import math
+import os, sys
 ##Clase de entrada en tabla
 class EntradaTabla:
     pid = -1
@@ -30,6 +31,7 @@ class AdministradorMemoria:
         for i in range(0, 500):
             datos = Datos(-1, '0')
             self.ram.append(datos)
+            datos = Datos(-1, '0')
             self.swap.append(datos)
 
     def agregar_proceso(self, pid, tam):
@@ -99,33 +101,111 @@ class AdministradorMemoria:
             if contador.__mod__(25) == 0:
                 print(' 0x'+str(contador))
 
+    def mover_ram(self, pid):
+        for entry in self.tabla:
+            if entry.pid == pid:
+                if entry.unidad != 'swap':
+                    print('El proceso no esta en SWAP')
+                    return -3
+                inicio = self.obtener_segmento_libre(entry.size, self.ram)
+                if inicio == -1:
+                    print('No hay suficiente memoria RAM')
+                    return -1
+                self.tabla.remove(entry)
+                datos = self.ram[entry.dir_fisica_inicio].datos
+                self.descargar_de_unidad(entry, self.swap)
+                entry.unidad = 'ram'
+                foo = EntradaTabla(entry.pid,inicio,entry.size, 'ram')
+                self.tabla.append(foo)
+                self.cargar_en_unidad(foo,self.ram)
+                self.cargar_datos_en_unidad(foo, datos, self.ram)
+                return 0
+        print('No se encontro el proceso')
+        return -2
 
+    def mover_swap(self, pid):
+        for entry in self.tabla:
+            if entry.pid == pid:
+                if entry.unidad != 'ram':
+                    print('El proceso no esta en RAM')
+                    return -3
+                inicio = self.obtener_segmento_libre(entry.size, self.swap)
+                if inicio == -1:
+                    print('No hay suficiente memoria SWAP')
+                    return -1
+                self.tabla.remove(entry)
+                datos = self.ram[entry.dir_fisica_inicio].datos
+                self.descargar_de_unidad(entry, self.ram)
+                entry.unidad = 'swap'
+                foo = EntradaTabla(entry.pid, inicio, entry.size, 'swap')
+                self.tabla.append(foo)
+                self.cargar_en_unidad(foo, self.swap)
+                self.cargar_datos_en_unidad(foo, datos, self.swap)
+                return 0
+        print('No se econtro el proceso')
+        return -2
 
 ##Aqui se implementara una maquina de estados que recibe
 #ordenes desde una fifo
 if __name__ == '__main__':
+    path = "/tmp/com1"
+    path2 = "/tmp/com2"
     ADMIN = AdministradorMemoria()
     I = True
     while I:
-        entrada_input = input('prompt > ')
-        if entrada_input == 'crear':
-            n_pid = int(input('pid > '))
-            tam_kb = int(input('tam > '))
-            ADMIN.agregar_proceso(n_pid, tam_kb)
-        elif entrada_input == 'matar':
-            n_pid = int(input('pid > '))
-            ADMIN.matar_proceso(n_pid)
-        elif entrada_input == 'insertar':
-            n_pid = int(input('pid > '))
-            data = input('datos > ')
-            ADMIN.insertar_datos(n_pid, data)
-        elif entrada_input == 'salir':
-            break
-        elif entrada_input == 'mover_swap':
-            n_pid = int(input('pid > '))
-            
-        elif entrada_input == 'dibujar':
-            print('::Memoria RAM::')
-            ADMIN.dibujar(ADMIN.ram)
-            print('::Memoria SWAP::')
-            ADMIN.dibujar(ADMIN.swap)
+        
+        fifo = open(path,'r')
+        entrada_input = fifo.read().split(':')
+        fifo.close()
+        os.system('clear')
+        if entrada_input[0] == 'hay_memoria':
+            REQUEST = -1
+            T = ADMIN.convertir_tamanio(int(entrada_input[1]))
+            if entrada_input[2] == 'ram':
+                REQUEST = ADMIN.obtener_segmento_libre(T, ADMIN.ram)
+            elif entrada_input[2] == 'swap':
+                REQUEST = ADMIN.obtener_segmento_libre(T, ADMIN.swap)
+            with open(path2, 'w') as f:
+                f.write(str(REQUEST))
+        elif entrada_input[0] == 'crear':
+            ADMIN.agregar_proceso(int(entrada_input[1]), int(entrada_input[2]))
+        elif entrada_input[0] == 'insertar':
+            ADMIN.insertar_datos(int(entrada_input[1]), entrada_input[2])
+        elif entrada_input[0] == 'matar':
+            ADMIN.matar_proceso(int(entrada_input[1]))
+        elif entrada_input[0] == 'mover_swap': 
+            REQUEST = ADMIN.mover_swap(int(entrada_input[1]))
+            with open(path2, 'w') as f:
+                f.write(str(REQUEST))
+        elif entrada_input[0] == 'mover_ram':
+            REQUEST = ADMIN.mover_ram(int(entrada_input[1]))
+            with open(path2, 'w') as f:
+                f.write(str(REQUEST))
+
+        print('\n\n:::RAM:::')
+        ADMIN.dibujar(ADMIN.ram)
+        print('\n\n:::SWAP:::')
+        ADMIN.dibujar(ADMIN.swap)
+        # # print(entrada_input)
+        #   entrada_input = input('prompt > ')
+        # if entrada_input == 'crear':
+        #     n_pid = int(input('pid > '))
+        #     tam_kb = int(input('tam > '))
+        #     ADMIN.agregar_proceso(n_pid, tam_kb)
+        # elif entrada_input == 'matar':
+        #     n_pid = int(input('pid > '))
+        #     ADMIN.matar_proceso(n_pid)
+        # elif entrada_input == 'insertar':
+        #     n_pid = int(input('pid > '))
+        #     data = input('datos > ')
+        #     ADMIN.insertar_datos(n_pid, data)
+        # elif entrada_input == 'salir':
+        #     break
+        # elif entrada_input == 'mover_swap':
+        #     n_pid = int(input('pid > '))
+        #     ADMIN.mover_swap(n_pid)
+        # elif entrada_input == 'dibujar':
+        #     print('::Memoria RAM::')
+        #     ADMIN.dibujar(ADMIN.ram)
+        #     print('::Memoria SWAP::')
+        #     ADMIN.dibujar(ADMIN.swap)
