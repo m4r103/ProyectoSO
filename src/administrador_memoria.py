3ac2edd8fdd11e7ce57243bbdc0030b4bb449e85
m4r103 <mariolopez.lopez66@gmail.com>
2017-12-05 23:31:08 -0600

    Nuevas funciones agregadas.
    Se ha agrregado la función de dibujar la ram, matar procesos y cargar a ram.

--- src/administrador_memoria.py
+++ src/administrador_memoria.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 import math
-
+##Clase de entrada en tabla
 class EntradaTabla:
     pid = -1
     dir_fisica_inicio = -1
@@ -12,7 +12,7 @@ class EntradaTabla:
         self.pid = pid
         self.size = size
         self.dir_fisica_inicio = dir_fisica
-        self.dir_fisica_fin = dir_fisica + (size-1)
+        self.dir_fisica_fin = dir_fisica + size
     
     def get_pid(self):
         return self.pid
@@ -28,7 +28,7 @@ class EntradaTabla:
     def set_unidad(self, unidad):
         self.unidad = unidad
 
-
+##Clase de Datos
 class Datos:
     def __init__(self, pid, datos):
         self.pid = pid
@@ -42,27 +42,33 @@ class Datos:
     def get_datos(self):
         return self.datos
 
+##Clase del administrador
 class AdministradorMemoria:
     tabla = []
     ram = []
     swap = []
     def __init__(self):
         for i in range(0, 500):
-            datos = Datos(-1, ' ')
+            datos = Datos(-1, '0')
             self.ram.append(datos)
             self.swap.append(datos)
 
     def agregar_proceso(self, pid, tam):
         tam = self.convertir_tamanio(tam)
-        if tam < 500 and self.tabla.count == 0:
-            self.tabla.append(EntradaTabla(pid, 0, tam))
-            return
         inicio = self.obtener_segmento_libre(tam)
-        if inicio != -1:
-            self.tabla.append(EntradaTabla(pid,inicio,tam))
-            return
-        else:
-            print('No hay memoria suficiente')    
+        if inicio == -1:
+            print('No hay memoria suficiente')
+            return 
+        entrada = EntradaTabla(pid, inicio, tam)
+        self.tabla.append(entrada)
+        self.cargar_en_ram(entrada)
+        
+    
+    def cargar_en_ram(self,entrada):
+        for i in range(entrada.dir_fisica_inicio,entrada.dir_fisica_fin):
+            self.ram[i].pid = entrada.get_pid()
+            self.ram[i].datos = entrada.get_pid()
+        
 
     def convertir_tamanio(self, tam):
         return math.ceil(float(tam/4))
@@ -75,29 +81,51 @@ class AdministradorMemoria:
                 cont = 0
                 inicio = i
                 for j in range(inicio, 500):
-                    if self.ram[j].get_pid() != -1:
-                        break
                     cont = cont+1
+                    if self.ram[j].get_pid() != -1 or cont == tam:
+                        break
             if cont == tam:
                 return inicio
         return -1
 
+    def matar_proceso(self, pid):
+        for entry in self.tabla:
+            if entry.pid == pid:
+                self.descargar_de_ram(entry)
+                self.tabla.remove(entry)
+                return
+        print('Proceso no econtrado')
+
+    def descargar_de_ram(self, entry):
+        for i in range(entry.dir_fisica_inicio, entry.dir_fisica_fin):
+            self.ram[i].datos = '0'
+            self.ram[i].pid = -1
+
     def dibujar(self):
-        for entry in self.ram:
-            print(entry.datos)
+        contador = 0
+        for entry in self.ram :
+            print(' '+entry.datos,end='')
+            contador =  contador + 1
+            if contador.__mod__(25) == 0:
+                print(' 0x'+str(contador))
 
 
 
-
-        
+##Aqui se implementara una maquina de estados que recibe
+#ordenes desde una fifo
 if __name__ == '__main__':
     ADMIN = AdministradorMemoria()
     I = True
     while I:
         entrada = input('prompt > ')
         if entrada == 'crear':
-            pid = int(input('pid >'))
-            tam = int(input('tam >'))
+            pid = int(input('pid > '))
+            tam = int(input('tam > '))
             ADMIN.agregar_proceso(pid,tam)
+        elif entrada == 'matar':
+            pid = int(input('pid > '))
+            ADMIN.matar_proceso(pid)
         elif entrada == 'salir':
             break
+        elif entrada == 'dibujar':
+            ADMIN.dibujar()
